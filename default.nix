42ca3938cd5c1d97b5360e514df70361308eec03
Jean-Philippe Braun <eon@patapon.info>
2022-06-16 15:07:22 +0200

    Add missing kubectl dependency

--- default.nix
+++ default.nix
@@ -5,39 +5,44 @@ pkgs ? import <nixpkgs> {}
 with pkgs;
 with pkgs.lib;
 with python3Packages;
+
 buildPythonApplication rec {
     pname = "rswitch";
     version = "1.2.1";
-    unpackPhase = ":";
-    phases= ["installPhase" "fixupPhase"];
+
     propagatedBuildInputs = [
-      (if stdenv.hostPlatform.isDarwin
-        then
-	  ""
-        else
-          firefox-unwrapped
-      )
+      (if stdenv.hostPlatform.isDarwin 
+       then ""
+       else firefox-unwrapped)
       geckodriver
       (python3.withPackages (pythonPackages: with pythonPackages; [ 
         requests
         selenium
         click
       ]))
+      kubectl
     ]; 
+
     src=".";
+
     buildInputs = [
       makeWrapper
     ];
+
+    unpackPhase = ":";
+    phases= [ "installPhase" "fixupPhase" ];
+
     installPhase = ''
       mkdir -p $out/bin 
       install -m755 -D ${./rswitch.py} $out/bin/rswitch
       substituteInPlace $out/bin/rswitch --replace RSWITCH_VERSION ${version}
     '';
+
     meta = with pkgs.lib; { 
-    description = "rswitch"; 
-    homepage = "https://github.com/Caascad/rswitch"; 
-    license = licenses.mit; 
-    maintainers = with maintainers; [ "karim-oueslati" ]; 
-  }; 
+      description = "rswitch"; 
+      homepage = "https://github.com/Caascad/rswitch"; 
+      license = licenses.mit; 
+      maintainers = with maintainers; [ "karim-oueslati" ]; 
+    }; 
 
   }
