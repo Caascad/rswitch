b5bf96735ac5d4f4bdc20f7bd7b3fe00738eed5c
Xavier Maillard <xavier.maillard@orange.com>
2022-09-14 19:14:21 +0200

    build: override nix pname & name

--- default.nix
+++ default.nix
@@ -5,26 +5,32 @@ pkgs ? import <nixpkgs> {}
 with pkgs;
 with pkgs.lib;
 with python39Packages;
-poetry2nix.mkPoetryApplication rec {
-  projectDir = ./.;
-  python = pkgs.python39;
-
-  propagatedBuildInputs = [
-    (if stdenv.hostPlatform.isDarwin
-      then
-    ""
-      else
-        firefox-unwrapped
-    )
-    geckodriver
-  ];
-  meta = with pkgs.lib; {
-    description = "rswitch";
-    homepage = "https://github.com/Caascad/rswitch";
-    license = licenses.mit;
-    maintainers = with maintainers; [ "abryko" ];
+let 
+  rswitch = poetry2nix.mkPoetryApplication rec {
+    projectDir = ./.;
+    python = pkgs.python39;
+  
+    propagatedBuildInputs = [
+      (if stdenv.hostPlatform.isDarwin
+        then
+      ""
+        else
+          firefox-unwrapped
+      )
+      geckodriver
+    ];
+    meta = with pkgs.lib; {
+      description = "rswitch";
+      homepage = "https://github.com/Caascad/rswitch";
+      license = licenses.mit;
+      maintainers = with maintainers; [ "abryko" ];
+    };
   };
-}
+in rswitch.overrideAttrs (old: rec {
+  pname = "rswitch";
+  version = old.version;
+  name = "${pname}-${version}";
+})
 
 
 
